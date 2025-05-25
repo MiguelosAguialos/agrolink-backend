ea11585cae8a7b9eba0e419299bb1418179ad237
MiguelosAguialos <aguiarmiguel65@gmail.com>
2025-05-24 21:06:00 -0300

    feat: add consumidor user data funcitions

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,41 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
-const consumidorService = require('../services/consumidor.service')
-const voluntarioService = require('../services/voluntario.service')
-const commonService = require('../services/common.service')
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const consumidorService = require("../services/consumidor.service");
+const voluntarioService = require("../services/voluntario.service");
+const commonService = require("../services/common.service");
 
-const port = 3000
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/newConsumidor', (req, res) => {
-    const user = req.body.user
-    res.send(consumidorService.createUserConsumidor(user))
-})
+app.post("/newConsumidor", (req, res) => {
+  const user = req.body.user;
+  res.send(consumidorService.createUserConsumidor(user));
+});
 
-app.post('/newVoluntario', (req, res) => {
-    const user = req.body.user
-    res.send(voluntarioService.createUserVoluntario(user))
-})
+app.post("/newVoluntario", (req, res) => {
+  const user = req.body.user;
+  res.send(voluntarioService.createUserVoluntario(user));
+});
 
-app.post('/login', (req, res) => {
-    const user = req.body.user
-    res.send(commonService.login(user))
-})
+app.post("/login", (req, res) => {
+  const user = req.body.user;
+  res.send(commonService.login(user));
+});
+
+app.post("/getConsumidorUserInfo", (req, res) => {
+  const email = req.body.email;
+  res.send(consumidorService.getUserConsumidor(email));
+});
+
+app.post("/updateConsumidor", (req, res) => {
+  const user = req.body.user;
+  res.send(consumidorService.updateConsumidor(user));
+});
 
 app.listen(port, () => {
-    console.log(`Application running on port: ${port}`)
-})
+  console.log(`Application running on port: ${port}`);
+});
--- src/services/consumidor.service.js
+++ src/services/consumidor.service.js
@@ -1,48 +1,121 @@
-const fs = require('fs')
-const AuthException = require('../exceptions/AuthException')
-const { checkUserVoluntario, checkUserConsumidor } = require('./common.service')
+const fs = require("fs");
+const AuthException = require("../exceptions/AuthException");
+const {
+  checkUserVoluntario,
+  checkUserConsumidor,
+} = require("./common.service");
 
 function formatUser(user, fields) {
-    const formatedUser = {}
-    for (field of fields) {
-        formatedUser[field] = user[field]
-    }
-    return formatedUser
+  const formatedUser = {};
+  for (field of fields) {
+    formatedUser[field] = user[field];
+  }
+  return formatedUser;
 }
 
 function createUserConsumidor(user) {
-    try {
-        const formatedUser = formatUser(user, [
-            "nome", "email", "celular", "password", "endereco", "complemento", "CEP", "qtdPessoas",
-            "preferenciasAlimentares", "restricoesAlimentares"
-        ])
-        checkUser(formatedUser)
-        return { message: 'Usuário criado com sucesso!', code: 200 }
-    } catch (err) {
-        return { title: err.title, message: err.message, code: err.code }
-    }
+  try {
+    const formatedUser = formatUser(user, [
+      "nome",
+      "email",
+      "celular",
+      "password",
+      "endereco",
+      "complemento",
+      "CEP",
+      "qtdPessoas",
+      "preferenciasAlimentares",
+      "restricoesAlimentares",
+    ]);
+    checkUser(formatedUser);
+    return { message: "Usuário criado com sucesso!", code: 200 };
+  } catch (err) {
+    return { title: err.title, message: err.message, code: err.code };
+  }
 }
 
 function checkUser(formatedUser) {
-    try {
-        jsonData = checkUserConsumidor(formatedUser)
-        checkUserVoluntario(formatedUser)
-        for (user of jsonData) {
-            if (formatedUser['email'] == user['email']) {
-                throw new AuthException('Erro ao cadastrar', 'Este email já está em uso.', 400)
-            }
-        }
-        jsonData.push(formatedUser)
-        fs.writeFile('src/storage/consumidores.json', JSON.stringify(jsonData), err => {
-            if (err) console.error(err)
-            console.log('Arquivo atualizado com sucesso!')
-        })
-    } catch (err) {
-        throw new AuthException(err.title, err.message, err.code)
+  try {
+    jsonData = checkUserConsumidor(formatedUser);
+    checkUserVoluntario(formatedUser);
+    for (user of jsonData) {
+      if (formatedUser["email"] == user["email"]) {
+        throw new AuthException(
+          "Erro ao cadastrar",
+          "Este email já está em uso.",
+          400
+        );
+      }
     }
+    jsonData.push(formatedUser);
+    fs.writeFile(
+      "src/storage/consumidores.json",
+      JSON.stringify(jsonData),
+      (err) => {
+        if (err) console.error(err);
+        console.log("Arquivo atualizado com sucesso!");
+      }
+    );
+  } catch (err) {
+    throw new AuthException(err.title, err.message, err.code);
+  }
+}
+
+function getUserConsumidor(email) {
+  try {
+    const fileData = fs.readFileSync("src/storage/consumidores.json");
+    let jsonData = [];
+    try {
+      jsonData = JSON.parse(fileData);
+    } catch (err) {
+      jsonData = [];
+    }
+    for (user of jsonData) {
+      if (email == user["email"]) {
+        console.log(user);
+        return user;
+      }
+    }
+  } catch (err) {
+    throw new AuthException("Erro para usuário", "Erro ao editar usuário", 500);
+  }
+}
+
+function updateConsumidor(mainUser) {
+  console.log(mainUser);
+  try {
+    const fileData = fs.readFileSync("src/storage/consumidores.json");
+    let jsonData = [];
+    try {
+      jsonData = JSON.parse(fileData);
+    } catch (err) {
+      jsonData = [];
+    }
+    jsonData.map((user, index) => {
+      if (mainUser["email"] == user["email"]) {
+        jsonData[index] = mainUser;
+      }
+    });
+    fs.writeFile(
+      "src/storage/consumidores.json",
+      JSON.stringify(jsonData),
+      (err) => {
+        if (err) console.error(err);
+        console.log("Arquivo atualizado com sucesso!");
+      }
+    );
+  } catch (err) {
+    throw new AuthException(
+      "Erro para usuário",
+      "Erro ao buscar usuário pelo email",
+      404
+    );
+  }
 }
 
 module.exports = {
-    formatUser,
-    createUserConsumidor
-}
+  formatUser,
+  createUserConsumidor,
+  getUserConsumidor,
+  updateConsumidor,
+};
